702be2ee88a0fab36f09b3dda8253307069265f9
Lauiee <132264578+Lauiee@users.noreply.github.com>
2025-10-01 03:39:54 +0900

    feat: user엔티티 구현

--- src/main/java/com/salang/backend/domain/user/entity/User.java
+++ src/main/java/com/salang/backend/domain/user/entity/User.java
@@ -3,15 +3,20 @@ package com.salang.backend.domain.user.entity;
 import com.salang.backend.global.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
 
 @Entity
@@ -19,7 +24,8 @@ import org.hibernate.annotations.SQLRestriction;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-@SQLRestriction("is_deleted = false") // 이걸 붙여야 소프트 딜리트 실행, delete 쿼리 발생 시 update 쿼리로 대체
+@SQLRestriction("deleted_at IS NULL")
+@SQLDelete(sql = "UPDATE users SET deleted_at = now() WHERE user_id = ?")
 @Table(name = "users")
 public class User extends BaseEntity {
 
@@ -28,6 +34,44 @@ public class User extends BaseEntity {
     @Column(name = "user_id")
     private Long id;
 
-    @Column(name = "user_name")
-    private String name;
+    @Column(name = "nickname", length = 20, nullable = false)
+    private String nickname;
+
+    @Column(name = "birth_date", nullable = false)
+    private LocalDate birthDate;
+
+    @Column(name = "gender", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @Column(name = "role", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    @Column(name = "region", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Region region;
+
+    @Column(name = "phone_number", nullable = false, length = 20)
+    private String phoneNumber;
+
+    @Column(name = "reputation", nullable = false)
+    private int reputation;
+
+    @Column(name = "social_type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private SocialType socialType;
+
+    @Column(name = "social_id")
+    private String socialId;
+
+    @Column(name = "status", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @Column(name = "profile_image_url")
+    private String profileImage;
+
+    @Column(name = "ci_hmac", nullable = false)
+    private String ciHmac;
 }
