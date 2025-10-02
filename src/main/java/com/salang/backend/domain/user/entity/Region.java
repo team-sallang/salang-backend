41157000ca6894e91154b4b38fa7e146ff3cabb1
Lauiee <132264578+Lauiee@users.noreply.github.com>
2025-10-02 19:31:07 +0900

    feat: region enum 수정

--- src/main/java/com/salang/backend/domain/user/entity/Region.java
+++ src/main/java/com/salang/backend/domain/user/entity/Region.java
@@ -1,5 +1,12 @@
 package com.salang.backend.domain.user.entity;
 
 public enum Region {
-    SEOUL;
+    SEOUL("서울특별시"),
+    BUSAN("부산광역시");
+
+    private final String displayName;
+
+    Region(String displayName){ this.displayName = displayName; }
+
+    public String getDisplayName() { return displayName; }
 }
